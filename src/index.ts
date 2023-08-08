dc82d3477dc6bc890a8e8b1ccbd08fe8793470c8
Jesper Madsen <jam@umbraco.dk>
2023-08-08 14:05:01 +0200

    change dependency

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import { getInput, setFailed, info, warning } from '@actions/core';
-import { startDeployment } from './ucStartDeploymentApi';
+import { ApiClient } from '@jam-test-umbraco/umbraco-cloud-deployment-apiclient';
 
 async function run() {
     try {
-        const projectAlias = getInput('project-alias');
-        const apiKey = getInput('api-key');
-        const deploymentId = getInput('deployment-id');
+        const apiBaseUrl = getInput('api-base-url', {required: true });
+        const projectAlias = getInput('project-alias', { required: true});
+        const apiKey = getInput('api-key', { required: true});
+        const deploymentId = getInput('deployment-id', { required: true});
+        
+        const client = new ApiClient(apiBaseUrl, projectAlias, apiKey);
 
-        const url = `https://api-internal.umbraco.io/projects/${projectAlias}/deployments/${deploymentId}`
-
-        const deployment = await startDeployment(url, apiKey);
+        const deployment = await client.startDeployment(deploymentId);
         info("Deployment accepted and queued to start shortly");
 
     } catch (error) {    
